7350f1b25b055f420f827a8f52c77ae893d22b44
Hao-Ting Wang <htwangtw@gmail.com>
2022-03-01 15:35:17 -0500

    CIMAQ patterm

--- giga_preprocess/extract_timeseries_tar.py
+++ giga_preprocess/extract_timeseries_tar.py
@@ -18,18 +18,27 @@ import pandas as pd
 import numpy as np
 from nilearn.image import load_img
 from nilearn.maskers import NiftiLabelsMasker, NiftiMapsMasker
+from nilearn.connectome import ConnectivityMeasure
 from nilearn.masking import compute_brain_mask
+import sklearn
 from templateflow import api as tflow
 from templateflow import conf as tf_conf
 
 
-DATASETS = ["adni_preprocess", "aibl_preprocess", "ccna_preprocess",
-            "cimaq_preprocess", "oasis_preprocess", "preventad_preprocess"]
+DATASETS = ["adni_preprocess", "ccna_preprocess", "cimaq_preprocess",
+            "oasis_preprocess", "preventad_preprocess"]
 
-TEMPLATEFLOW_HOME = "./data/raw/templateflow"
+TEMPLATEFLOW_HOME = "./data/raw/customed_templateflow"
 INPUT_DIR = "/data/cisl/giga_preprocessing/preprocessed_data"
 OUTPUT_ROOT = '/data/cisl/preprocessed_data/giga_timeseries/'
-NIAK_PATTERN = r"fmri_sub(?P<sub>[A-Za-z0-9]*)_sess(?P<ses>[A-Za-z0-9]*)_task(?P<task>[A-Za-z0-9]{,4})(run(?P<run>[0-9]{2}))?"
+NIAK_PATTERN = {
+    'adin': r"fmri_sub(?P<sub>[A-Za-z0-9]*)_sess(?P<ses>[A-Za-z0-9]*)_task(?P<task>[A-Za-z0-9]{,4})(run(?P<run>[0-9]{2}))?",
+    'ccna': r"fmri_sub(?P<sub>[A-Za-z0-9]*)_sess(?P<ses>[A-Za-z0-9]*)_task(?P<task>[A-Za-z0-9]{,4})(run(?P<run>[0-9]{2}))?",
+    'cimaq': r"fmri_sub(?P<sub>[A-Za-z0-9]*)_(?P<task>[A-Za-z0-9]{,4})_(run(?P<run>[0-9]{1}))?",
+    'oasis': r"fmri_sub(?P<sub>[A-Za-z0-9]*)_sess(?P<ses>[A-Za-z0-9]*)_task(?P<task>[A-Za-z0-9]{,4})(run(?P<run>[0-9]{2}))?",
+    'preventad': r"fmri_sub(?P<sub>[A-Za-z0-9]*)_sess(?P<ses>[A-Za-z0-9]*)_task(?P<task>[A-Za-z0-9]{,4})(run(?P<run>[0-9]{2}))?",
+}
+
 NIAK_CONFOUNDS = ["motion_tx", "motion_ty", "motion_tz",
                   "motion_rx", "motion_ry", "motion_rz",
                   "wm_avg", "vent_avg", "slow_drift"]
@@ -45,17 +54,7 @@ ATLAS_METADATA = {
         'atlas_parameters': ['resolution', 'desc'],
         'label_parameters': ['desc'],
         },
-    'difumo': {
-        'source': "templateflow",
-        'templates' : ['MNI152NLin2009cAsym', 'MNI152NLin6Asym'],
-        'resolutions': [2, 3],
-        'atlas': 'DifuMo',
-        'description_pattern': "{dimension}dimensions",
-        'dimensions': [64, 128, 256, 512, 1024],
-        'atlas_parameters': ['resolution', 'desc'],
-        'label_parameters': ['resolution','desc'],
-        },
-    "segmented_difumo": {
+    'segmented_difumo': {
         'source': "user_define",
         'templates' : ['MNI152NLin2009cAsym'],
         'resolutions': [2, 3],
@@ -65,7 +64,7 @@ ATLAS_METADATA = {
         'atlas_parameters': ['resolution', 'desc'],
         'label_parameters': ['resolution','desc'],
         },
-    "mist": {
+    'mist': {
         'source': "user_define",
         'templates' : ['MNI152NLin2009bSym'],
         'resolutions': [3],
@@ -85,12 +84,12 @@ def get_parser():
 
     parser.add_argument(
         "-d", "--dataset", required=True, help=""
-        "NIAK giga processing dataset {adni, aibl, ccna, cimaq, oasis, preventad}",
+        "NIAK giga processing dataset {adni, ccna, cimaq, oasis, preventad}",
     )
 
     parser.add_argument(
         "-a", "--atlas", required=False, default=-1, help=""
-        "Atlas for timeseries extraction. Valid resolutions available are {Schaefer2018, DiFuMo}, -1 for all (default: -1)",
+        "Atlas for timeseries extraction. Valid resolutions available are {schaefer7, mist, segmented_difumo}, -1 for all (default: -1)",
     )
     return parser
 
@@ -132,17 +131,15 @@ def fetch_atlas_path(atlas_name, template, resolution, description_keywords):
     cur_atlas_meta = ATLAS_METADATA[atlas_name].copy()
 
     if cur_atlas_meta['source'] != "templateflow":
-        templateflow.conf.init_layout()
+        tf_conf.init_layout()
 
     img_parameters = generate_templateflow_parameters(cur_atlas_meta, "atlas", resolution, description_keywords)
     label_parameters = generate_templateflow_parameters(cur_atlas_meta, "label", resolution, description_keywords)
-    img_path = templateflow.api.get(template, raise_empty=True, **img_parameters)
+    img_path = tflow.get(template, raise_empty=True, **img_parameters)
     img_path = str(img_path)
-    label_path = templateflow.api.get(template, raise_empty=True, **label_parameters)
+    label_path = tflow.get(template, raise_empty=True, **label_parameters)
 
     labels = pd.read_csv(label_path, delimiter="\t")
-    # labels = (labels['Region'].astype(str) + ". " +
-    #           labels['Difumo_names']).values.tolist()
     atlas_type = img_path.split('_')[-1].split('.nii.gz')[0]
     return sklearn.utils.Bunch(maps=img_path, labels=labels, type=atlas_type)
 
@@ -208,26 +205,31 @@ def create_atlas_masker(atlas_name, description_keywords, template='MNI152NLin20
                              template=template,
                              description_keywords=description_keywords)
 
+    atlas_desc = atlas.maps.split('desc-')[-1].split('_')[0]
+
     if atlas.type == 'dseg':
-        masker = nilearn.input_data.NiftiLabelsMasker(atlas.maps, detrend=True)
+        masker = NiftiLabelsMasker(atlas.maps, detrend=True)
     elif atlas.type == 'probseg':
-        masker = nilearn.input_data.NiftiMapsMasker(atlas.maps, detrend=True)
+        masker = NiftiMapsMasker(atlas.maps, detrend=True)
     if nilearn_cache:
         masker = masker.set_params(memory=nilearn_cache, memory_level=1)
     labels = list(range(1, atlas.labels.shape[0] + 1))
-    return masker, labels
+    return masker, labels, atlas_desc
 
 
-def niak2bids(niak_filename):
+def niak2bids(niak_filename, dataset_name):
     """Parse niak file name to BIDS entities."""
     print("\t" + niak_filename)
-    compile_name = re.compile(NIAK_PATTERN)
+    compile_name = re.compile(NIAK_PATTERN[dataset_name])
     return compile_name.search(niak_filename).groupdict()
 
 
 def bidsish_timeseries_file_name(file_entitiles):
     """Create a BIDS-like file name to save extracted timeseries as tsv."""
-    base = f"sub-{file_entitiles['sub']}_ses-{file_entitiles['ses']}_task-{file_entitiles['task']}_"
+    base = f"sub-{file_entitiles['sub']}_"
+    if  file_entitiles.get("ses", False) is not None:
+        base += "ses-{file_entitiles['ses']}_"
+    base += "task-{file_entitiles['task']}_"
     if  file_entitiles.get("run", False) is not None:
         base += f"run-{file_entitiles['run']}_"
     return base
@@ -255,7 +257,6 @@ def temporal_derivatives(data):
     """Compute first order temporal derivative terms by backwards differences."""
     data_deriv = np.tile(np.nan, data.shape)
     data_deriv[1:] = np.diff(data, n=1, axis=0)
-
     return data_deriv
 
 
@@ -294,8 +295,12 @@ def load_niak_confounds(fmri_path):
 def create_timeseries_root_dir(output_dir, file_entitiles):
     """Create root directory for the timeseries file."""
     subject = f"sub-{file_entitiles['sub']}"
-    session = f"ses-{file_entitiles['ses']}" if file_entitiles.get(
-        'ses', False) is not None else None
+    if file_entitiles.get('ses', False):
+        if file_entitiles.get('ses', False) is not None:
+            session = f"ses-{file_entitiles['ses']}"
+    else:
+        session = None
+
     if session:
         timeseries_root_dir = output_dir / subject / session
     else:
@@ -311,20 +316,32 @@ if __name__ == '__main__':
     nilearn_cache = ""
     args = get_parser().parse_args()
     atlas_names = ATLAS_METADATA.keys() if args.atlas == -1 else [args.atlas]
-    dataset = f"{args.dataset}_preprocess"
+    dataset = args.dataset
+    dataset_dir = f"{dataset}_preprocess"
+    tf_conf.TF_HOME = Path(TEMPLATEFLOW_HOME)
+    tf_conf.update(local=True)
 
-    print("#### {} ####".format(dataset))
-    preprocessed_data_dir = input_dir / dataset / "resample"
+    print("#### {} ####".format(dataset_dir))
+    preprocessed_data_dir = input_dir / dataset_dir / "resample"
+    dataset_name = f"dataset-{dataset}"
+    output_dir = output_root_dir / dataset_name
+    output_dir.mkdir(parents=True, exist_ok=True)
+    fmri_data = preprocessed_data_dir.glob("*.nii.gz")
+    fmri_data = list(fmri_data)
+    print(fmri_data[0])
+
     for atlas_name in atlas_names:
         print("-- {} --".format(atlas_name))
-        dataset_name = f"dataset-{dataset.replace('_preprocess', '')}_atlas-{atlas_name}"
-        output_dir = output_root_dir / dataset_name
-        output_dir.mkdir(parents=True, exist_ok=True)
-        fmri_data = preprocessed_data_dir.glob("*.nii.gz")
-        fmri_data = list(fmri_data)
+
+        if atlas_name == "mist":
+            template = "MNI152NLin2009bSym"
+            resolution = 3
+        else:
+            template = "MNI152NLin2009cAsym"
+            resolution = 2
 
         for fmri_path in fmri_data:
-            file_entitiles = niak2bids(fmri_path.name)
+            file_entitiles = niak2bids(fmri_path.name, dataset)
             confounds = load_niak_confounds(fmri_path)
             if isinstance(confounds, int):
                 continue
@@ -334,14 +351,12 @@ if __name__ == '__main__':
             mask_img = compute_brain_mask(fmri_nii, memory=nilearn_cache)
 
             for dimension in ATLAS_METADATA[atlas_name]['dimensions']:
-                print("\t" + parcel)
+                print(f"\t{dimension}")
                 description_keywords = {"dimension": dimension}
-                masker, labels = create_atlas_masker(
+                masker, labels, atlas_desc = create_atlas_masker(
                         atlas_name, description_keywords, template=template, resolution=resolution)
                 output_filename_root = bidsish_timeseries_file_name(
                     file_entitiles)
-                atlas_desc = label_path.name.split('desc-')[-1].split('_')[0]
-
                 masker.set_params(mask_img=mask_img)
                 raw_tseries = masker.fit_transform(
                     str(fmri_path))
@@ -352,6 +367,12 @@ if __name__ == '__main__':
                         f"atlas-{atlas_name}{atlas_desc}_desc-{desc}_timeseries.tsv"
                     timeseries = pd.DataFrame(tseries, columns=labels)
                     timeseries.to_csv(timeseries_output_dir / output_filename, sep='\t', index=False)
+                corr_measure = ConnectivityMeasure(kind="correlation")
+                connectome = corr_measure.fit_transform([clean_tseries])[0]
+                connectome = pd.DataFrame(connectome, columns=labels, index=labels)
+                connectome.to_csv(
+                    timeseries_output_dir / output_filename.replace("timeseries", "connectome"),
+                    sep='\t')
         # tar the dataset
         with tarfile.open(output_root_dir / f"{dataset_name}.tar.gz", "w:gz") as tar:
             tar.add(output_dir, arcname=output_dir.name)
@@ -363,5 +384,8 @@ def test_niak2bids():
     """Check niak name parser."""
     case_1 = "fmri_sub130S5006_sess20121114_taskrestrun01_n.nii.gz"
     case_2 = "fmri_sub130S5006_sess20121114_taskrest_n.nii.gz"
-    assert niak2bids(case_1).get("run", False) == "01"
-    assert niak2bids(case_2).get("run", False) is None
+    case_cimaq = "fmri_sub164965_rest_run1_n.nii.gz"
+    assert niak2bids(case_1, 'adin').get("run", False) == "01"
+    assert niak2bids(case_2, 'adin').get("run", False) is None
+    assert niak2bids(case_cimaq, 'cimaq').get("run", False) == "1"
+    assert niak2bids(case_cimaq, 'cimaq').get("ses", False) is False
