44e7690031c4ec5daf12a58860e582b7a81872bd
Hao-Ting Wang <htwangtw@gmail.com>
2022-03-11 11:07:50 -0500

    Update bash script for running without a scheduler

--- giga_timeseres.sh
+++ giga_timeseres.sh
@@ -1,5 +1,7 @@
 GIGA_DIR='/data/cisl/giga_preprocessing/preprocessed_data'
 OUTPUT_ROOT='/data/cisl/preprocessed_data/giga_timeseries'
+OUTPUT_ROOT='/data/cisl/preprocessed_data/giga_fix'
+
 datasets='adni ccna cimaq oasis'
 
 cd ~/simexp/hwang/giga_preprocess_scripts
@@ -15,26 +17,28 @@ echo "get all processes to be checked"
 #     done;
 # done
 
-# split -d -l 200 all_files.tsv batch_files/batch_ 
-# split -d -l 300 all_files.tsv batch_files/difumo_batch_ 
+split -d -l 120 all_files.tsv batch_files/batch_ 
 
 echo "Created batch files"
 
+for i in $(seq 0 44); do
+    printf -v j "%02d" $i
+    i=$((i+1))
+    echo $i
+    while read -r line; do
+        IFS=$'\t' read dataset file_basename atlas <<< $line
+        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a mist >> logs/$dataset.$file_basename.log
+        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a schaefer >> logs/$dataset.$file_basename.log
+    done < batch_files/batch_$j &
+done
+
+split -d -l 200 all_files.tsv batch_files/difumo_batch_ 
 for i in $(seq 0 26); do
     printf -v j "%02d" $i
     i=$((i+1))
     echo $i
     while read -r line; do
         IFS=$'\t' read dataset file_basename atlas <<< $line
-        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a mist 
-        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a schaefer
-    done < batch_files/batch_$j > logs/subject_batch_$j.log &
-done
-
-for i in $(seq 0 5); do
-    printf -v j "%02d" $i
-    while read -r line; do
-        IFS=$'\t' read dataset file_basename atlas <<< $line
-        taskset --cpu-list 3$i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a segmented_difumo 
-    done < batch_files/difumo_batch_$j > logs/difumo_batch_$j.log &
+        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a segmented_difumo >> logs/$dataset.$file_basename.log
+    done < batch_files/difumo_batch_$j &
 done
