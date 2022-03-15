36692a030bb5f814b072ae17bb0e13558239123b
Hao-Ting Wang <htwangtw@gmail.com>
2022-03-15 14:24:19 -0400

    FIX path and use difumo as the basis for splitting batches

--- giga_timeseres.sh
+++ giga_timeseres.sh
@@ -1,6 +1,5 @@
 GIGA_DIR='/data/cisl/giga_preprocessing/preprocessed_data'
 OUTPUT_ROOT='/data/cisl/preprocessed_data/giga_timeseries'
-OUTPUT_ROOT='/data/cisl/preprocessed_data/giga_fix'
 
 datasets='adni ccna cimaq oasis'
 
@@ -21,17 +20,6 @@ split -d -l 120 all_files.tsv batch_files/batch_
 
 echo "Created batch files"
 
-for i in $(seq 0 44); do
-    printf -v j "%02d" $i
-    i=$((i+1))
-    echo $i
-    while read -r line; do
-        IFS=$'\t' read dataset file_basename atlas <<< $line
-        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a mist >> logs/$dataset.$file_basename.log
-        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a schaefer >> logs/$dataset.$file_basename.log
-    done < batch_files/batch_$j &
-done
-
 split -d -l 200 all_files.tsv batch_files/difumo_batch_ 
 for i in $(seq 0 26); do
     printf -v j "%02d" $i
@@ -39,6 +27,6 @@ for i in $(seq 0 26); do
     echo $i
     while read -r line; do
         IFS=$'\t' read dataset file_basename atlas <<< $line
-        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT -a segmented_difumo >> logs/$dataset.$file_basename.log
+        taskset --cpu-list $i python giga_preprocess/giga_timeseries.py -i $GIGA_DIR -d $dataset -s $file_basename -o $OUTPUT_ROOT >> logs/$dataset.$file_basename.log
     done < batch_files/difumo_batch_$j &
 done
